cdf87451b6124dbb9f8a7b40025f163649b3e19c
devKire <erikdossantos2006@outlook.com>
2024-11-01 16:33:16 -0300

    pixel tentativa 07

--- sale-page/src/App.js
+++ sale-page/src/App.js
@@ -1,4 +1,6 @@
 import "./styles.css";
+import { Helmet } from 'react-helmet';
+
 import DropdownList from "./components/DropdownlList";
 import Footer from "./components/Footer";
 
@@ -7,32 +9,29 @@ import Features from "./components/Features";
 function App() {
   return (
     <div className="App">
-      {/* Meta Pixel Code */}
-      <script>
+      <Helmet>
+        <title>Receitas para papais e mamães de pet</title>
+        <meta name="description" content="Receitas fáceis e baratas para alimentar seu cãozinho de forma saudável." />
+        <script>
           {`
-            !function(f,b,e,v,n,t,s)
-            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-            n.queue=[];t=b.createElement(e);t.async=!0;
-            t.src=v;s=b.getElementsByTagName(e)[0];
-            s.parentNode.insertBefore(t,s)}(window, document,'script',
-            'https://connect.facebook.net/en_US/fbevents.js');
+            !function(f,b,e,v,n,t,s){
+              if(f.fbq)return;n=f.fbq=function(){
+                n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)
+              };
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
             fbq('init', '567918892420888');
             fbq('track', 'PageView');
             fbq('track', 'ViewContent');
-
           `}
         </script>
-        <noscript>
-          <img height="1" width="1" style={{display: 'none'}}
-               src="https://www.facebook.com/tr?id=567918892420888&ev=PageView&noscript=1"
-          />
-        </noscript>
-      <head>
-        <title>Receitas para papais e mamães de pet</title>
-        <meta name="description" content="Receitas fáceis e baratas para alimentar seu cãozinho de forma saudável." />
-      </head>
+        <noscript dangerouslySetInnerHTML={{
+          __html: `<img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=567918892420888&ev=PageView&noscript=1"/>`
+        }} />
+      </Helmet>
       <header>
         <h1>
           Receitas que milhares de papais e mamães de pet estão utilizando para
